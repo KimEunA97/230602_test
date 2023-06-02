c9b61264488eeb02c5859c7fe2d4cf11fb377a44
KimEunA97 <kimeuna9712@gmail.com>
2023-06-02 16:47:19 +0900

    index.html로 번들러 파일 테스트

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const fs = require('fs')
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send('hello world');
+
 });
 
 
